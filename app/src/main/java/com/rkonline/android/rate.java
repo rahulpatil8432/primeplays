987c332b364b11372e120ae0c313d4aac7441e17
ketan <ketan@gmail.com>
2025-12-19 13:49:48 +0530

    Market Select Game UI changes.

--- app/src/main/java/com/rkonline/android/rate.java
+++ app/src/main/java/com/rkonline/android/rate.java
@@ -36,12 +36,7 @@ public class rate extends AppCompatActivity {
         super.setContentView(R.layout.activity_game);
         initView();
         headerTitle.setText(getIntent().getStringExtra("header"));
-        findViewById(R.id.back).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        findViewById(R.id.back).setOnClickListener(v -> finish());
         apicall();
     }
 
@@ -51,36 +46,33 @@ public class rate extends AppCompatActivity {
         progressDialog = new ViewDialog(rate.this);
         progressDialog.showDialog();
 
-        db.collection("games").orderBy("order").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                progressDialog.hideDialog();
-                if (task.isSuccessful()) {
-                    ArrayList<String> name = new ArrayList<>();
-                    ArrayList<String> rate = new ArrayList<>();
+        db.collection("games").orderBy("order").get().addOnCompleteListener(task -> {
+            progressDialog.hideDialog();
+            if (task.isSuccessful()) {
+                ArrayList<String> name = new ArrayList<>();
+                ArrayList<String> rate = new ArrayList<>();
 
 
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        Map<String, Object> userMap = document.getData();
-                        name.add((String) userMap.get("name"));
-                        rate.add((String) userMap.get("rate"));
-                    }
-                    adapter_game rc;
-                    Log.e("header",headerTitle.getText().toString());
-                    if(headerTitle.getText().toString().equalsIgnoreCase("Game Rate")){
-                       rc = new adapter_game(rate.this,name,rate);
-                        recyclerview.setLayoutManager(new LinearLayoutManager(rate.this));
-
-                    }else{
-                        rc = new adapter_game(rate.this,name,new ArrayList<>());
-                        recyclerview.setLayoutManager(new GridLayoutManager(rate.this,2));
-
-                    }
-                    recyclerview.setAdapter(rc);
-                    rc.notifyDataSetChanged();
-                } else {
-                    Toast.makeText(rate.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                for (QueryDocumentSnapshot document : task.getResult()) {
+                    Map<String, Object> userMap = document.getData();
+                    name.add((String) userMap.get("name"));
+                    rate.add((String) userMap.get("rate"));
                 }
+                adapter_game rc;
+                Log.e("header",headerTitle.getText().toString());
+                if(headerTitle.getText().toString().equalsIgnoreCase("Game Rate")){
+                   rc = new adapter_game(rate.this,name,rate);
+                    recyclerview.setLayoutManager(new LinearLayoutManager(rate.this));
+
+                }else{
+                    rc = new adapter_game(rate.this,name,new ArrayList<>());
+                    recyclerview.setLayoutManager(new GridLayoutManager(rate.this,2));
+
+                }
+                recyclerview.setAdapter(rc);
+                rc.notifyDataSetChanged();
+            } else {
+                Toast.makeText(rate.this, "Something went wrong", Toast.LENGTH_SHORT).show();
             }
         });
     }
