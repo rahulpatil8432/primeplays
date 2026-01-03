8152cad59b5823a60ab106019cfa46e1a7103592
Yash Jain <y.jain@t-systems.com>
2026-01-03 17:40:11 +0530

    adapter move to folder

--- app/src/main/java/com/rkonline/android/adapter/adaptertransaction.java
+++ app/src/main/java/com/rkonline/android/adapter/adaptertransaction.java
@@ -1,4 +1,4 @@
-package com.rkonline.android;
+package com.rkonline.android.adapter;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.rkonline.android.R;
 import com.rkonline.android.model.TransactionModel;
 
 import java.util.ArrayList;
--- app/src/main/java/com/rkonline/android/transactions.java
+++ app/src/main/java/com/rkonline/android/transactions.java
@@ -2,7 +2,6 @@ package com.rkonline.android;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View;
@@ -15,6 +14,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.rkonline.android.adapter.adaptertransaction;
 import com.rkonline.android.model.TransactionModel;
 
 import java.text.SimpleDateFormat;
